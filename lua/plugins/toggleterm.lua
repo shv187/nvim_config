8753c0d1658abaa6cbd60d70c722e34c6dca5ffa
shv187 <shv187@tutanota.com>
2025-04-21 01:22:22 +0200

    configure toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,13 @@
 return {
     'akinsho/toggleterm.nvim',
     version = '*',
-    opts = {},
+    opts = {
+        open_mapping = [[<c-\>]],
+        direction = 'float',
+        float_opts = {
+            border = 'single',
+            width = math.floor(vim.o.columns * 0.75),
+            height = math.floor(vim.o.lines * 0.75),
+        },
+    },
 }
