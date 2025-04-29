55554650d213b626c16771fbffe25147dfe8ebcc
shv187 <shv187@tutanota.com>
2025-04-29 22:33:59 +0200

    adjust nvim-ufo config

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,9 +1,19 @@
 return {
     'kevinhwang91/nvim-ufo',
     dependencies = { 'kevinhwang91/promise-async' },
-    config = function()
-        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-        vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+    opts = {
+        filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason' },
+    },
+    config = function(_, opts)
+        vim.api.nvim_create_autocmd('FileType', {
+            group = vim.api.nvim_create_augroup('local_detach_ufo', { clear = true }),
+            pattern = opts.filetype_exclude,
+            callback = function()
+                require('ufo').detach()
+            end,
+        })
+
+        vim.opt.foldlevelstart = 99
+        require('ufo').setup(opts)
     end,
 }
