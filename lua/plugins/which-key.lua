1905db42edfc597318d428f83fe649fa22c6ed70
shv187 <shv187@tutanota.com>
2025-04-30 04:20:23 +0200

    add buffer_local_keymaps keybind to which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -43,4 +43,13 @@ return {
             { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         },
     },
+    keys = {
+        {
+            '<leader>?',
+            function()
+                require('which-key').show({ global = false })
+            end,
+            desc = 'Buffer Local Keymaps (which-key)',
+        },
+    },
 }
