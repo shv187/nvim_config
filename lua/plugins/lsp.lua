f63c731f3dc5ebadbc7bf0e55c86c35f580cfe14
shv187 <shv187@tutanota.com>
2025-05-08 22:54:49 +0200

    switch to mason 2.0 and stop using mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,8 +12,8 @@ return {
     {
         'neovim/nvim-lspconfig',
         dependencies = {
-            { 'williamboman/mason.nvim', opts = {} },
-            'williamboman/mason-lspconfig.nvim',
+            { 'mason-org/mason.nvim', opts = {} },
+            'mason-org/mason-lspconfig.nvim',
             'WhoIsSethDaniel/mason-tool-installer.nvim',
             { 'j-hui/fidget.nvim', opts = {} },
             'saghen/blink.cmp',
@@ -109,8 +109,6 @@ return {
                 },
             })
 
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
-
             local servers = {
                 clangd = {
                     on_attach = function(_, bufnr)
@@ -152,10 +150,10 @@ return {
                 html = {},
                 htmx = {},
                 cssls = {},
+                antlersls = {},
             }
 
-            local ensure_installed = vim.tbl_keys(servers or {})
-            vim.list_extend(ensure_installed, {
+            local rest = {
                 'stylua',
                 'clang-format',
                 'codelldb',
@@ -166,20 +164,34 @@ return {
                 'black',
                 'htmlhint',
                 'prettier',
-            })
-            require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
+            }
 
-            require('mason-lspconfig').setup({
-                ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-                automatic_installation = false,
-                handlers = {
-                    function(server_name)
-                        local server = servers[server_name] or {}
-                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                        require('lspconfig')[server_name].setup(server)
-                    end,
-                },
-            })
+            local ensure_installed = vim.tbl_keys(servers or {})
+            vim.list_extend(ensure_installed, rest)
+
+            require('mason-tool-installer').setup({ ensure_installed = ensure_installed, debounce_hours = 24 })
+
+            -- local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+            -- nvim 0.11.0+
+            for server, config in pairs(servers) do
+                -- config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                vim.lsp.config(server, config)
+                vim.lsp.enable(server)
+            end
+
+            -- nvim <= 0.10.x
+            -- require('mason-lspconfig').setup({
+            --     ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+            --     automatic_enable = true,
+            --     handlers = {
+            --         function(server_name)
+            --             local server = servers[server_name] or {}
+            --             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            --             require('lspconfig')[server_name].setup(server)
+            --         end,
+            --     },
+            -- })
         end,
     },
 }
