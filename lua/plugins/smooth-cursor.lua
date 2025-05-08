c40deaae3cfbb1f3a0a031fbb00d5cca52fe1e2e
shv187 <shv187@tutanota.com>
2025-05-08 22:53:32 +0200

    adjust smooth-cursor

--- lua/plugins/smooth-cursor.lua
+++ lua/plugins/smooth-cursor.lua
@@ -1,10 +1,10 @@
 return {
     'sphamba/smear-cursor.nvim',
     opts = {
-        stiffness = 0.55, -- 0.6      [0, 1]
-        trailing_stiffness = 0.25, -- 0.3      [0, 1]
+        stiffness = 0.95, -- 0.6      [0, 1]
+        trailing_stiffness = 0.66, -- 0.3      [0, 1]
         distance_stop_animating = 0.1, -- 0.1      > 0
-        time_interval = 6,
+        time_interval = 7,
         smear_between_buffers = false,
     },
 }
