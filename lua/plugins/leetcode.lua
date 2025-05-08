d045ef9425eb0ddf38bb2092ca59e272ced1bce8
shv187 <shv187@tutanota.com>
2025-05-08 22:54:14 +0200

    set leetcode.lua to lazy

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,5 +1,6 @@
 return {
     'kawre/leetcode.nvim',
+    lazy = true,
     -- build = ':TSUpdate html', -- if you have `nvim-treesitter` installed
     dependencies = {
         'nvim-telescope/telescope.nvim',
