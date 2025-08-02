c6bcf16b7875b5bb9671945c105ea0d592bd6d32
shv187 <shv187@tutanota.com>
2025-08-02 19:09:43 +0200

    change discord-rpc plugin

--- lua/plugins/discord-rpc.lua
+++ lua/plugins/discord-rpc.lua
@@ -1,4 +1,16 @@
 return {
-    'andweeb/presence.nvim',
-    event = 'VeryLazy',
+    'vyfor/cord.nvim',
+    build = ':Cord update',
+    config = function()
+        require('cord').setup({
+            plugins = {
+                'cord.plugins.diagnostics', -- Enable the diagnostics plugin
+
+                ['cord.plugins.diagnostics'] = { -- Configure diagnostics plugin
+                    scope = 'workspace', -- Set scope to 'workspace' instead of default 'buffer'
+                    severity = vim.diagnostic.severity.WARN, -- Show warnings and above
+                },
+            },
+        })
+    end,
 }
